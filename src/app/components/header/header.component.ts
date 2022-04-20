e448f98f69e1b8f18439e263b4097c4d8f74c32e
GyJonatan <gyurkovicsjonatan@hotmail.com>
2022-04-20 11:23:09 +0200

    first commit

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  title: string = 'angular-practice';
+  showAddTask!: boolean;
+  subscription!: Subscription;
+
+  constructor(private uiService:UiService) { }
+
+  ngOnInit(): void { }
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
+
+}
